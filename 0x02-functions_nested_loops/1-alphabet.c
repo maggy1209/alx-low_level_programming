3979e05b65969e4840e9976870524d8d7825d881
maggy1209 <otienom807@gmail.com>
2023-06-20 06:16:08 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - print the alphabet in lowercase followed by a new line
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
-program that prints _putchar, followed by a new line.
+1. program that prints _putchar, followed by a new line.
+
+2. function that prints the alphabet, in lowercase, followed by a new line
