8cf7700532586893b692354401e6a3af7c9144e2
maggy1209 <otienom807@gmail.com>
2023-06-20 00:57:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*main - main block
+*Description: prints _putchar
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+	char c[] = "_putchar";
+
+	for (i = 0; j < 5; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+* `Task 0 :`program that prints __putchar, followed by a new line.
