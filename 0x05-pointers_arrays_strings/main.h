f0bbbd9b8d5cb91ef1d0e9e8c9101dc72fe414e9
maggy1209 <otienom807@gmail.com>
2023-06-26 00:21:29 -0700

    header file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+1.void reset_to_98(int *n);
+2.void swap_int(int *a, int *b);
+3.int _strlen(char *s);
+4. void _puts(char *str);
+5. void print_rev(char *s);
+6.void rev_string(char *s);
+7.void puts2(char *str);
+8.void puts_half(char *str);
+9.void print_array(int *a, int n);
+10.char *_strcpy(char *dest, char *src);
+
