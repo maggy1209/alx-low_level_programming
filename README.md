4d61951abfbfd77c9d33efd37082d92815343338
maggy1209 <otienom807@gmail.com>
2023-06-29 00:56:26 -0700

    header file

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * print_remaining_days()
 * 
 alx-low_level_programming
+0x06-pointers_arrays_strings
--- main.h
+++ main.h
@@ -9,4 +9,12 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);
+void print_number(int)
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+1337.
