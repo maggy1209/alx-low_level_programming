f2a2a5571c3d8ed7ea3f055fc507616f53d1a299
maggy1209 <otienom807@gmail.com>
2023-07-13 05:48:15 -0700

    function that creates an array of integers.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*array_range - creates an array of integers
+*@min: first integer number
+*@max: last integer number
+*Return: pointer to the memory, NULL if it fails
+*/
+int *array_range(int min, int max)
+{
+	int *a;
+	int i, j;
+
+	if (min > max)
+		return (NULL);
+	j = max - min + 1;
+	a = malloc(sizeof(int) * j);
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++, min++)
+		a[i] = min;
+	return (a);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -3,5 +3,5 @@ Task `0` : function that allocates memory using malloc.
 * Returns a pointer to the allocated memory
 * if malloc fails, the malloc_checked function should cause normal process termination with a status value 
 1. function that concatenates two strings.
-2. unction that allocates memory for an array, using malloc.
-:
+2. function that allocates memory for an array, using malloc.
+3. function that creates an array of integers.
