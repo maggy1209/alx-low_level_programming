ed7f2d4efbba5fed32569d305646f1f802d86422
maggy1209 <otienom807@gmail.com>
2023-07-18 00:35:11 -0700

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - main function
+*Description: prints out the file name its compiled from
+*Return: 0 on success
+*/
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
